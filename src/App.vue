89fc4a489be571ac11cbf351811450029309f166
Juan Sebastián González López <juangonzalezlopez241829@gmail.com>
2022-03-16 17:00:27 +0100

    añadiendo funcionalidades

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
-    <current-time/>
+    <current-time class="col-4"/>
+    <task-input class="col-6"/>
   </div>
 </template>
 
 <script>
 import CurrentTime from "@/components/CurrentTime";
+import TaskInput from "@/components/TaskInput";
 export default {
   name: 'app',
-  components: {CurrentTime},
+  components: {TaskInput, CurrentTime},
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import './style.css'
 import App from './App.vue'
+
 
 createApp(App).mount('#app')
